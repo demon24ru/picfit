9201d06f906549b4ea619d2c8c637b42a96f5bfd
Dmitry <ittek@inbox.ru>
2020-12-12 17:00:29 +0700

    upd parsers.go

--- middleware/parsers.go
+++ middleware/parsers.go
@@ -16,8 +16,7 @@ import (
 	"github.com/thoas/picfit/util"
 )
 
-var parametersReg = regexp.MustCompile(`(?:(?P<sig>\w+))?/?(?P<path>[\w\-/.]+)`)
-var parametersOpsReg = regexp.MustCompile(`(?P<op>\w+)/(?:(?P<w>\d+))?x(?:(?P<h>\d+))?/(?P<path>[\w\-/.]+)`)
+var parametersReg = regexp.MustCompile(`(?:(?P<sig>\w+)/)?(?P<op>\w+)/(?:(?P<w>\d+))?x(?:(?P<h>\d+))?/(?P<path>[\w\-/.]+)`)
 
 // ParametersParser matches parameters to query string
 func ParametersParser() gin.HandlerFunc {
@@ -31,26 +30,10 @@ func ParametersParser() gin.HandlerFunc {
 
 				results := parametersReg.SubexpNames()
 
-				var path string
 				for i, name := range results {
 					if i != 0 && match[i] != "" {
 						parameters[name] = match[i]
-						if name == "path" {
-							path = match[i]
-						}
 						fmt.Printf("Pars parameter name=%v value=%v i=%v\n", name, match[i], i)
-					}
-				}
-
-				match = parametersOpsReg.FindStringSubmatch(path)
-				if match != nil {
-					resultsPath := parametersOpsReg.SubexpNames()
-
-					for i, name := range resultsPath {
-						if i != 0 && match[i] != "" {
-							parameters[name] = match[i]
-							fmt.Printf("Pars parameter name=%v value=%v i=%v\n", name, match[i], i)
-						}
 					}
 				}
 
